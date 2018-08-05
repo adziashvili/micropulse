045cb392c6c234c3e534392c8342634a7b5eab45
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-08-05 22:53:52 +0800

    Built a basic model for pipeline.

--- src/pipeline/model/pipelineStore.js
+++ src/pipeline/model/pipelineStore.js
@@ -9,6 +9,10 @@ export default class PipelineStore {
 
     constructor() {
         this.store = []
+        this.stages = [
+          "Technical Validation",
+          "Business Validation",
+          "Committed" ]
     }
 
     initialise( data ) {
@@ -39,8 +43,6 @@ export default class PipelineStore {
                 pr._stage,
                 pr._pa ] )
         } )
-
-        console.log( "PIPE STORE INTIALISED", this.store.length );
     }
 
     set store( store ) {
@@ -59,8 +61,84 @@ export default class PipelineStore {
         return this.store.length
     }
 
+    get names() {
+        return this._names
+    }
+
     build( names, date ) {
-        // here we need to build models
+
+        if ( !!names ) {
+            this._names = names
+        }
+        if ( !!date ) {
+            this._date = date
+        }
+
+        if ( this.store.length === 0 ) {
+            // nothing to build
+            return
+        }
+
+        this.setupMonths()
+        // Scan and see what is the min and max pipe months
+
+        this.monthly = this.buildMonthlyPipeline()
+        // Build the base monthly pipeline data
+
+        this.monthly.forEach( ( p ) => {
+            console.log( p.practice );
+            p.months.forEach( ( m ) => {
+                console.log( m );
+            } )
+        } )
+    }
+
+    buildMonthlyPipeline() {
+        let model = []
+
+        // This builds the totals per month
+        this.names.forEach( ( name ) => {
+
+            let pipe = { "practice": name, "months": [] }
+            model.push( pipe )
+
+            this.months.forEach( ( m ) => {
+                let monthlyPipe = { "month": m }
+                pipe.months.push( monthlyPipe )
+                let total = { value: 0, count: 0 }
+                this.stages.forEach( ( s ) => {
+                    monthlyPipe[ s ] = this.sumPipe( this.practices( name ), m, s, "_value" )
+                    total.value = this.add( total.value, monthlyPipe[ s ].value )
+                    total.count += monthlyPipe[ s ].count
+                } )
+                monthlyPipe[ "Total" ] = total
+                monthlyPipe[ "Total" ].avg = this.average( total.value, total.count )
+            } )
+        } )
+
+        // Building the total per stage per practice
+        model.forEach( ( p ) => {
+
+            let practiceTotalPipe = { "month": -1 }
+            let pTotals = { value: 0, count: 0, avg: 0 }
+
+            this.stages.forEach( ( s ) => {
+                let pStageTotals = { value: 0, count: 0, avg: 0 }
+                practiceTotalPipe[ s ] = pStageTotals
+                p.months.forEach( ( m ) => {
+                    pStageTotals.value = this.add( pStageTotals.value, m[ s ].value )
+                    pStageTotals.count += m[ s ].count
+                    pTotals.value = this.add( pTotals.value, m[ s ].value )
+                    pTotals.count += m[ s ].count
+                } )
+            } )
+
+            pTotals.avg = this.average( pTotals.value, pTotals.count )
+            practiceTotalPipe[ "Total" ] = pTotals
+            p.months.push( practiceTotalPipe )
+        } )
+
+        return model
     }
 
     reconcile( data ) {
@@ -88,5 +166,102 @@ export default class PipelineStore {
         } )
 
         return filterlist.length > 0
+    }
+
+    setupMonths() {
+        this.minDate = this.minCloseDate()
+        this.maxDate = this.maxCloseDate()
+        this.months = []
+
+        for ( let m = this.minDate.getMonth(); m <= this.maxDate.getMonth(); m++ ) {
+            this.months.push( m )
+        }
+    }
+
+    maxCloseDate() {
+        let maxDate = null
+
+        if ( this.store.length > 0 ) {
+            maxDate = this.store[ 0 ].closeDate
+        } else {
+            return maxDate
+        }
+
+        this.store.forEach( ( p ) => {
+            if ( p.closeDate.valueOf() > maxDate.valueOf() ) {
+                maxDate = p.closeDate
+            }
+        } )
+
+        return maxDate
+    }
+
+    minCloseDate() {
+        let minDate = null
+
+        if ( this.store.length > 0 ) {
+            minDate = this.store[ 0 ].closeDate
+        } else {
+            return minDate
+        }
+
+        this.store.forEach( ( p ) => {
+            if ( p.closeDate.valueOf() < minDate.valueOf() ) {
+                minDate = p.closeDate
+            }
+        } )
+
+        return minDate
+    }
+
+    practices( name ) {
+
+        let APAC = [ "ANZ", "ASEAN", "INDIA", "S.KOREA" ]
+
+        switch ( name ) {
+            case "APAC":
+                return APAC
+                break;
+            case "APJ":
+                return APAC.concat( [ "JAPAN", "APJ Shared" ] )
+                break;
+            default:
+                return [ name ]
+        }
+    }
+
+    average( sum, count ) {
+        return count !== 0 ? this.fixed( sum / count ) : 0
+    }
+
+    add( a, b ) {
+        return this.fixed( a + b )
+    }
+
+    fixed( num ) {
+        return num.toFixed( 2 ) * 1
+    }
+
+    sumPipe( practices, month, stage, propery ) {
+
+        let pipe = this.store.filter( ( p ) => {
+            return practices.includes( p.practice ) &&
+                p.closeDate.getMonth() === month &&
+                p.stage === stage
+        } )
+
+        let value = this.sum( pipe, propery )
+        let count = pipe.length
+        let avg = this.average( value, count )
+
+        return { value, count, avg }
+    }
+
+    sum( list, property ) {
+        let sum = 0
+        list.forEach( ( item ) => {
+            sum += item[ property ]
+        } )
+        return sum.toFixed( 2 ) * 1
     }
 }
