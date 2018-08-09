ac6497c3d1bbc29ae67e63d684188aca94b0a8d6
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-08-10 04:35:40 +0800

    Pipeline store build up with rollups of PA and AI

--- src/pipeline/model/pipelineStore.js
+++ src/pipeline/model/pipelineStore.js
@@ -89,65 +89,102 @@ export default class PipelineStore {
         this.monthly = this.buildMonthlyPipeline()
         // Build the base monthly pipeline data
 
+        this.buildMonthlyRollups()
+        // Per month per stage, calculates rollups for Total, AI and PA
+
+        this.buildTotals()
+        // Add totals to the monthly model (i.e. total pipe by stage).
+        // This also rolls up the various stats like PA and AI
+
         this.buildMonthlyRatios()
-        // Building the monthly pipe in respect to total
+        // Building the monthly pipe in respect to total pipe value
     }
 
     buildMonthlyPipeline() {
+
         let model = []
-
-        // This builds the totals per month
         this.names.forEach( ( name ) => {
-
             let pipe = { "practice": name, "months": [] }
             model.push( pipe )
-
-            this.months.forEach( ( m ) => {                
-                let monthlyPipe = { "month": m.month, isPast: m.isPast }
-                pipe.months.push( monthlyPipe )
-                let total = { value: 0, count: 0 }
+            this.months.forEach( ( m ) => {
+                let mp = { "month": m.month, isPast: m.isPast }
                 this.stages.forEach( ( s ) => {
-                    monthlyPipe[ s ] = this.sumPipe( this.pm.expand( name ), m.month, s,
-                        "_value" )
-                    total.value = this.add( total.value, monthlyPipe[ s ].value )
-                    total.count += monthlyPipe[ s ].count
+                    mp[ s ] = this.stats( this.pm.expand( name ), m.month, s )
                 } )
-                monthlyPipe[ "Total" ] = total
-                monthlyPipe[ "Total" ].avg = this.average( total.value, total.count )
+                pipe.months.push( mp )
             } )
-        } )
-
-        // Building the total per stage per practice
-        model.forEach( ( p ) => {
-
-            let practiceTotalPipe = { "month": -1 }
-            let pTotals = { value: 0, count: 0, avg: 0 }
-
-            this.stages.forEach( ( s ) => {
-                let pStageTotals = { value: 0, count: 0, avg: 0 }
-                practiceTotalPipe[ s ] = pStageTotals
-                p.months.forEach( ( m ) => {
-                    pStageTotals.value = this.add( pStageTotals.value, m[ s ].value )
-                    pStageTotals.count += m[ s ].count
-                    pTotals.value = this.add( pTotals.value, m[ s ].value )
-                    pTotals.count += m[ s ].count
-                } )
-            } )
-
-            pTotals.avg = this.average( pTotals.value, pTotals.count )
-            practiceTotalPipe[ "Total" ] = pTotals
-            p.months.push( practiceTotalPipe )
         } )
 
         return model
+    }
+
+    buildMonthlyRollups() {
+        this.monthly.forEach( ( p ) => {
+            p.months.forEach( ( m ) => {
+                let rollups = this.rollup( [ m ] )
+                m.pa = rollups.pa
+                m.ai = rollups.ai
+                m.Total = rollups.t
+            } )
+        } )
+    }
+
+    rollup( ms = [] ) {
+        let pa = { count: 0, vsCount: 0 } // Partner Attach
+        let ai = { value: 0, count: 0, avg: 0, vsValue: 0, vsCount: 0 } // Adoption Incentive
+        let t = { value: 0, count: 0, avg: 0 } // Total that cuts across stage values
+
+        ms.forEach( ( m ) => {
+            if ( m.month === -1 ) { return } // we should not rollup the last month
+            this.stages.forEach( ( s ) => {
+                pa.count += m[ s ].pa.count
+                ai.value = this.add( ai.value, m[ s ].ai.value )
+                ai.count += m[ s ].ai.count
+                t.value = this.add( t.value, m[ s ].value )
+                t.count += m[ s ].count
+            } )
+        } )
+
+        pa.vsCount = this.divide( pa.count, t.count )
+        ai.avg = this.divide( ai.value, ai.count )
+        ai.vsValue = this.divide( ai.value, t.value )
+        ai.vsCount = this.divide( ai.count, t.count )
+        t.avg = this.divide( t.value, t.count )
+
+        return { pa, ai, t }
+    }
+
+    buildTotals() {
+        this.monthly.forEach( ( p ) => {
+
+            let pt = {
+                "month": -1,
+                isPast: false,
+            }
+
+            this.stages.forEach( ( s ) => {
+                pt[ s ] = { value: 0, count: 0, avg: 0 }
+                p.months.forEach( ( m ) => {
+                    pt[ s ].value = this.add( pt[ s ].value, m[ s ].value )
+                    pt[ s ].count += m[ s ].count
+                } )
+            } )
+
+            let rollups = this.rollup( p.months )
+            pt.pa = rollups.pa
+            pt.ai = rollups.ai
+            pt.Total = rollups.t
+
+            p.months.push( pt )
+        } )
     }
 
     buildMonthlyRatios() {
         this.monthly.forEach( ( p ) => {
             let total = this.getPipe( p.practice, -1, "Total" )
             p.months.forEach( ( m ) => {
-                let monthlyTotal = m[ "Total" ].value
-                m.monthlyVsTotal = total !== 0 ? monthlyTotal / total : 0
+                m.monthlyVsTotal = this.divide( m.Total.value / total )
+
             } )
         } )
     }
@@ -213,19 +250,19 @@ export default class PipelineStore {
         }
     }
 
-    average( sum, count ) {
-        return count !== 0 ? this.fixed( sum / count ) : 0
-    }
-
     add( a, b ) {
         return this.fixed( a + b )
+    }
+
+    divide( x, y ) {
+        return ( x / ( y === 0 ? 1 : y ) ).toFixed( 3 )
     }
 
     fixed( num ) {
         return num.toFixed( 2 ) * 1
     }
 
-    sumPipe( practices, month, stage, propery ) {
+    stats( practices, month, stage ) {
 
         let pipe = this.store.filter( ( p ) => {
             return practices.includes( p.practice ) &&
@@ -233,11 +270,48 @@ export default class PipelineStore {
                 p.stage === stage
         } )
 
-        let value = this.sum( pipe, propery )
+        let value = this.sum( pipe, "_value" )
         let count = pipe.length
-        let avg = this.average( value, count )
+        let avg = this.divide( value, count )
+        // Stage top line stats
 
-        return { value, count, avg }
+        let pa = this.statsPa( pipe, count )
+        let ai = this.statsAi( pipe, value, count )
+        // Stage extra stats
+
+        return { value, count, avg, pa, ai }
+    }
+
+    statsPa( pipe, count ) {
+        let pa = this.countIf( pipe, "_pa", ( i ) => { return i } )
+        return {
+            count: pa,
+            vsCount: ( pa / ( count === 0 ? 1 : count ) ).toFixed( 3 ) * 1
+        }
+    }
+
+    statsAi( pipe, value, count ) {
+
+        let ai = {
+            value: 0,
+            count: 0,
+            avg: 0,
+            vsCount: 0,
+            vsValue: 0
+        }
+
+        pipe.forEach( ( p ) => {
+            if ( p.ai > 0 ) {
+                ai.value += p.ai
+                ai.count++
+            }
+        } )
+
+        ai.avg = this.divide( ai.value, ai.count )
+        ai.vsCount = this.divide( ai.count, count )
+        ai.vsValue = this.divide( ai.value, value )
+
+        return ai
     }
 
     sum( list, property ) {
@@ -246,5 +320,15 @@ export default class PipelineStore {
             sum += item[ property ]
         } )
         return sum.toFixed( 2 ) * 1
+    }
+
+    countIf( list, property, evaluator ) {
+        let count = 0
+        list.forEach( ( item ) => {
+            if ( evaluator( item[ property ] ) ) {
+                count++
+            }
+        } )
+        return count
     }
 }
