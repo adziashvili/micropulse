8e18d2a36a02356e9dc1ca8625fd4468dc2b7e97
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-08-31 23:43:21 +0800

    adjusted layout

--- src/modules/bookings/bookingsPulse.js
+++ src/modules/bookings/bookingsPulse.js
@@ -17,8 +17,8 @@ export default class BookingsPulse extends Report {
 
   setup() {
     const bookingsKey = 'Amount (converted)'
-    this.firstColShrinkBy = 5
-    this.otherColShrinBy = 5
+    this.firstColShrinkBy = 3
+    this.otherColShrinBy = 3
 
     this.dictionary = new Dictionary([
       { key: 'TOTAL', shortName: 'APJ' },
