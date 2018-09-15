df4ab01391d3a0562a44f505549195efeccfbfcb
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-09-15 19:22:03 +0800

    Added documentaiton to Analyzer. Moved it back to sfdc folder since it is sfdc specfic.

--- src/common/sfdc/analyzer.js
+++ src/common/sfdc/analyzer.js
@@ -39,36 +39,73 @@ export default class Analyzer {
     }
   }
 
+  /**
+   * Tests that all array elements are strings (typeof value === string)
+   *
+   * @param {Array} data Elemtns to test
+   *
+   * @return {Boolean} True if all are strings.
+   */
   static isString(data) {
     return !data.some(d => !(typeof d === 'string'))
   }
 
+  /**
+   * Tests that all values are currencies.
+   *
+   * Currency currently is detected by a prefix of 'USD '
+   *
+   * @param {Array]} data Array of values to test
+   *
+   * @return {Boolean} True if all values start with 'USD '
+   */
   static isCurrency(data) {
     const sizeOfDashOrNull = data.filter(d => Statistics.ZERO_VALUES.includes(d)).length
-
     if (sizeOfDashOrNull === data.length) {
       return false
     }
-
     return !data.filter(d => !Statistics.ZERO_VALUES.includes(d))
       .some(d => !(typeof d === 'string' && d.toLowerCase().trim().startsWith('usd ')))
   }
 
+  /**
+   * Tests if an array of values includes all numbers.
+   *
+   * @param {Array} data Array of data
+   *
+   * @return {Boolean} true if all values are numbers. Otherwise false.
+   */
   static isNumber(data) {
     const sizeOfDashOrNull = data.filter(d => Statistics.ZERO_VALUES.includes(d)).length
-
     if (sizeOfDashOrNull === data.length) {
       return false
     }
-
     return !data.filter(d => !Statistics.ZERO_VALUES.includes(d))
       .some(d => typeof d !== 'number')
   }
 
+  /**
+   * Warning: Very naive test for percentage.
+   *
+   * Tests an array of values to see if all values are pecentages.
+   *
+   * @param {Array} data Array of data.
+   *
+   * @return {Boolean} True if all values start with %.
+   */
   static isPercent(data) {
     return !data.some(d => !d.toLowerCase().endsWith('%'))
   }
 
+  /**
+   * Tests if an array of objects is an array of dates.
+   *
+   * @param {Array} data Array of objects or string representing a valid date.
+   *
+   * @return {Boolean} True if all array elemnts pass the following tests:
+   *                   Date.parse(value) returns a valid miliseconds.
+   *                   new Date (miliseconds) is an actual date.
+   */
   static isDate(data) {
     return !data.some((d) => {
       const ms = Date.parse(d)
@@ -83,13 +120,21 @@ export default class Analyzer {
     })
   }
 
+  /**
+   * Tests if all values of an array can be evalued to boolean.
+   * Tests includes yes, no, true, flase, 0 and 1
+   *
+   * @param {Array} data Array of values
+   *
+   * @return {Boolean} True if all values can be evaluetd to boolean
+   */
   static isBoolean(data) {
     return !data.some((d) => {
       if (typeof d === 'boolean') {
         return false
       }
       if (typeof d === 'string') {
-        return !(['yes', 'no', 'true', 'false'].includes(d.toLowerCase()))
+        return !(['1', '0', 'yes', 'no', 'true', 'false'].includes(d.toLowerCase()))
       }
       if (typeof d === 'number') {
         return !(d === 0 || d === 1)
